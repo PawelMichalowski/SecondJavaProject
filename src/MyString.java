7bfb4bce76bf9892078b6f8fc53c14e3c5327f5b
Pawel Michalowski <pawel.michalowski@gds.ey.com>
2023-03-23 09:02:56 +0100

    new start

---
+++ src/MyString.java
@@ -0,0 +1,16 @@
+public class MyString {
+    public static void main(String[] args) {
+        String hello = "Hello, Pawel";
+        System.out.println(hello);
+
+        int stringLength = hello.length();
+        System.out.println(stringLength);
+
+        String helloUp = hello.toUpperCase();
+        System.out.println(helloUp);
+
+        String helloLow = hello.toLowerCase();
+        System.out.println(helloLow);
+
+    }
+}
